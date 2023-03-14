c501ba7d02e2786f1d279e38af689f0a96ba542f
BarrazaBrenda <127805993+BarrazaBrenda@users.noreply.github.com>
2023-03-13 23:32:44 -0300

    Create DesafioDeCodigo_SubeteAEseJuego.js

---
+++ CLOUD COMPUTING - PRE-BOOTCAMP/DesafioDeCodigo_SubeteAEseJuego.js
@@ -0,0 +1,8 @@
+    var edad = 10;
+    var altura = 10;
+    
+    if(edad>9||altura>99){
+        console.log("¡Súbete, chico!");
+    }else{
+        console.log("Lo siento, chico. Tal vez el próximo año”);
+    }
