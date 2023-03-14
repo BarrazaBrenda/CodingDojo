5f2759b36a37676a50badc3a7c9586c0caa12be7
BarrazaBrenda <127805993+BarrazaBrenda@users.noreply.github.com>
2023-03-13 23:42:12 -0300

    Delete DesafioDeCodigo_SubeteAEseJuego.js

--- CLOUD COMPUTING - PRE-BOOTCAMP/DesafioDeCodigo_SubeteAEseJuego.js
+++
@@ -1,8 +0,0 @@
-    var edad = 10;
-    var altura = 10;
-    
-    if(edad>9||altura>99){
-        console.log("¡Súbete, chico!");
-    }else{
-        console.log("Lo siento, chico. Tal vez el próximo año”);
-    }
