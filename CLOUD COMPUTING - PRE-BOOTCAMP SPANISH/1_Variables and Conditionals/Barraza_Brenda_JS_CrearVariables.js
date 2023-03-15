680174bdb5a8d4944df9b0b07cffe9f917bbde97
BarrazaBrenda <127805993+BarrazaBrenda@users.noreply.github.com>
2023-03-14 21:39:12 -0300

    Create Barraza_Brenda_JS_CrearVariables.js

---
+++ CLOUD COMPUTING - PRE-BOOTCAMP SPANISH/1_Variables and Conditionals/Barraza_Brenda_JS_CrearVariables.js
@@ -0,0 +1,8 @@
+
+    var edad = 10;
+    var altura = 100;
+    
+    /*para que la persona pueda subirse se debe analizar los datos
+    que el usuario asigno a las variables y si estos cumplen por 
+    lo menos con ambos requisitos minimos (edad de 10 años y un 
+    Metro de Altura)*/
