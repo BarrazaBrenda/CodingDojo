9b5f0a2036ed956974983f69cbe0cb41298d5523
BarrazaBrenda <127805993+BarrazaBrenda@users.noreply.github.com>
2023-03-14 22:03:45 -0300

    Create predecirArreglosyCondicionales.js

---
+++ CLOUD COMPUTING - PRE-BOOTCAMP SPANISH/4_Arrays, Functions, and Returns/predecirArreglosyCondicionales.js
@@ -0,0 +1,30 @@
+Fragmento de código 1:
+
+
+auntContactInfo[0]    |       Paula
+auntContactInfo[1] | Herrero
+auntContactInfo[2] | 1234 Calle Principal
+auntContactInfo[3] | San Luis
+auntContactInfo[4]    |       MO
+auntContactInfo[5]    |       12345
+
+
+
+Fragmento de código 2:
+
+
+producir[0] | manzanas
+producir[1] | naranjas
+congelado[0] | Brócoli
+congelado[1] | helado
+congelado[2] | Croquetas de Papa
+
+
+Fragmento de código 3:
+
+
+movieLibrary    |   Bambi
+movieLibrary | Jugo de escarabajo
+movieLibrary    |   Toy Story
+movieLibrary    |   Zoro
+
