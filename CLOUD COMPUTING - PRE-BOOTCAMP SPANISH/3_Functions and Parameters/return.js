c29068a972d6b49c1040d3b78815461888cc1e75
BarrazaBrenda <127805993+BarrazaBrenda@users.noreply.github.com>
2023-03-15 12:12:54 -0300

    Create return.js

---
+++ CLOUD COMPUTING - PRE-BOOTCAMP SPANISH/3_Functions and Parameters/return.js
@@ -0,0 +1,95 @@
+Fragmento de código 1 
+function hello() {
+    console.log('hola');
+}
+hello();
+console.log('Dojo');
+
+
+Fragmento de código 2
+function hello() {
+    console.log('hola');
+    return 15;
+}
+var result = hello();
+console.log('el resultado es', result);
+
+
+Fragmento de código 3
+function numPlus(num) {
+    console.log('num es', num);
+    return num+15;
+}
+var result = numPlus(3);
+console.log('x es', result);
+
+
+Fragmento de código 4: hay un giro
+var num = 15;
+console.log(num);
+function logAndReturn(num){
+   console.log(num);   
+   return num;
+}
+var result = logAndReturn(10);
+console.log(result);
+console.log(num);
+
+
+Fragmento de código 5
+var num = 15;
+console.log(num);
+function timesTwo(num){
+   console.log(num);   
+   return num*2;
+}
+var result = timesTwo(10);
+console.log(result);
+console.log(num);
+
+
+Fragmento de código 6
+function timesTwoAgain(num) {
+    console.log('num es', num);
+    y = num*2;
+    return y;
+}
+var result = timesTwoAgain(3) + timesTwoAgain(5);
+console.log('el resultado es', result);
+
+
+Fragmento de código 7
+function sumNums(num1, num2) {  
+   return num1+num2;
+}
+console.log(sumNums(2,3))
+console.log(sumNums(3,5))
+
+
+Fragmento de código 8
+function printSumNums(num1, num2) {
+   console.log(num1);   
+   return num1+num2;
+}
+console.log(printSumNums(2,3))
+console.log(printSumNums(3,5))
+
+
+Fragmento de código 9
+function sumNums(num1, num2) {
+    var sum = num1 + num2;
+    console.log('sum es', sum);
+    return sum;
+}
+var result = sumNums(2,3) + sumNums(3,5);
+console.log('el resultado es', result);
+
+
+Fragmento de código 10
+function sumNums(num1, num2) {
+    var sum = num1 + num2;
+    console.log('sum es', sum);
+    return sum;
+}
+var result = sumNums(2,3) + sumNums(3,sumNums(2,1)) + sumNums(sumNums(2,1),sumNums(2,3));
+console.log('el resultado es', result);
