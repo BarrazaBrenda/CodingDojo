a29cf891b3634df30b147525ac283a1aa5a50de5
BarrazaBrenda <127805993+BarrazaBrenda@users.noreply.github.com>
2023-03-14 21:46:08 -0300

    Create característicaStretch_1.js

---
+++ CLOUD COMPUTING - PRE-BOOTCAMP SPANISH/1_Variables and Conditionals/Desafío de código Súbete a ese juego/característicaStretch_1.js
@@ -0,0 +1,10 @@
+    var altura = 100;
+    var edad=12;
+    
+    //si es mayor a 10 años
+    
+    if( altura>52 && edad>10 ){
+        console.log("¡Súbete, chico!");
+    }else{
+        console.log("Lo siento, chico. Tal vez el próximo año”);
+    }
