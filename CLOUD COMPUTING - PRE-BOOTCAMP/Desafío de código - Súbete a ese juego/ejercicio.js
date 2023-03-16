b603a2055ac9c10317bb52cf1e2d25b84ffc54bd
BarrazaBrenda <127805993+BarrazaBrenda@users.noreply.github.com>
2023-03-16 14:50:51 -0300

    Delete CLOUD COMPUTING - PRE-BOOTCAMP directory

--- CLOUD COMPUTING - PRE-BOOTCAMP/Desafío de código - Súbete a ese juego/ejercicio.js
+++
@@ -1,7 +0,0 @@
-    var altura = 100;
-    
-    if(altura>52){
-        console.log("¡Súbete, chico!");
-    }else{
-        console.log("Lo siento, chico. Tal vez el próximo año”);
-    }
--- CLOUD COMPUTING - PRE-BOOTCAMP/Desafío de código Parámetros/ejercicio.js
+++
@@ -1,6 +0,0 @@
-function greet(){
-    return "Anakin";
-}
-
-var nombre = greet();
-console.log("buenos dias", nombre);
